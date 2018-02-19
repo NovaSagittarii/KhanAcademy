f72f2b3899f1333758a3008a75dc235764b33872
Thomas Li <thomas1210@gmail.com>
2018-02-18 19:22:44 -0800

    array of points

--- [SNP] Teleporter.js
+++ [SNP] Teleporter.js
@@ -1,12 +1,29 @@
-
-var sensitivity = 3;
-mouseMoved = mouseDragged = function(){
-    mouseX = mouseX*sensitivity % width;
-    mouseY = mouseY*sensitivity % height;
+var nou = [];
+var teleport = 3;
+var E = 2;
+var No_U = function(){
+    this.x = random(0, width);
+    this.y = random(0, height);
+    this.px = this.x;
+    this.py = this.y;
+    this.vx = random(-E, E);
+    this.vy = random(-E, E);
+};
+No_U.prototype.work = function(){
+    this.px = this.x;
+    this.py = this.y;
+    this.x = ~~(width+this.x+this.vx*teleport) % width;
+    this.y = ~~(height+this.y+this.vy*teleport) % height;
+    return [this.x, this.y, this.px, this.py];
 };
 strokeWeight(5);
 stroke(0);
+for(var i = 0; i < 10; i ++){
+    nou.push(new No_U());
+}
 draw = function(){
     background(255, 255, 255);
-    line(pmouseX, pmouseY, mouseX, mouseY);
+    for(var i = 0; i < nou.length; i ++){
+        line.apply(null, nou[i].work());
+    }
 };
